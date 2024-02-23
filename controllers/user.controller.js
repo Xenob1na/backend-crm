6808d2a38ec1d5f2be5efa53fb7271a43362268f
Gromov Danil <105980448+Xenob1na@users.noreply.github.com>
2024-02-23 21:19:19 +0700

    Redesigned authorization and registration

---
+++ controllers/user.controller.js
@@ -0,0 +1,106 @@
+import { User } from "../models/user.model.js";
+
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+import "dotenv/config";
+
+export const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const userExist = await User.findOne({ where: { email } });
+
+    if (userExist) {
+      return res.status(400).json({ message: "Пользователь уже существует" });
+    }
+
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Пароль должен содержать не менее 6 символов" });
+    }
+
+    await User.create({
+      name,
+      email,
+      password: await bcrypt.hash(password, 10),
+    });
+
+    return res.status(200).json({ message: "Пользователь создан" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({ message: "Пожалуйста, введите все поля" });
+    }
+
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) {
+      return res.status(400).json({ message: "Пользователь не существует" });
+    }
+
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Пароль должен содержать не менее 6 символов" });
+    }
+
+    const passwordValid = await bcrypt.compare(password, user.password);
+
+    if (!passwordValid) {
+      return res.status(400).json({ message: "Неверные пароль или почта" });
+    }
+
+    const token = jwt.sign({ id: user.id }, process.env.TOKEN_SECRET, {
+      expiresIn: "1d",
+      algorithm: "HS256",
+    });
+
+    res.status(200).json({
+      data: {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+      },
+      token: token,
+      message: "Успешная авторизация",
+    });
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
+};
+
+export const getUser = async (req, res) => {
+  try {
+    if (
+      !req.headers.authorization ||
+      !req.headers.authorization.startsWith("Bearer") ||
+      !req.headers.authorization.split(" ")[1]
+    ) {
+      return res.status(422).json({
+        message: "Please provide the token",
+      });
+    }
+
+    const theToken = req.headers.authorization.split(" ")[1];
+    const decoded = jwt.verify(theToken, process.env.TOKEN_SECRET);
+
+    const user = await User.findOne({ where: { id: decoded.id } });
+
+
+    
+
+    console.log(user);
+    return res.status(200).json({ data: user });
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
+};
--- routes/routes.js
+++ routes/routes.js
@@ -1,18 +1,12 @@
 import express from "express";
-import db from "../config/database.js";
-import { signupValidation, loginValidation } from "../helpers/validation.js";
-
-import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
-
 
 import {
   getTasks,
   getTaskById,
   createTask,
   deleteTask,
-  deleteTaskAll
-} from "../controllers/task.controller.js"
+  deleteTaskAll,
+} from "../controllers/task.controller.js";
 
 import {
   getCustomers,
@@ -38,122 +32,17 @@ import {
   deleteSchedule,
 } from "../controllers/scheduleController.js";
 
+import {
+  registerUser,
+  loginUser,
+  getUser,
+} from "../controllers/user.controller.js";
+
 const router = express.Router();
 
-router.post("/register", signupValidation, (req, res, next) => {
-  db.query(
-    `SELECT * FROM users WHERE LOWER(email) = LOWER(${db.escape(
-      req.body.email
-    )});`,
-    (err, result) => {
-      if (result.length) {
-        return res.status(409).send({
-          msg: "This user is already in use!",
-        });
-      } else {
-        
-        bcrypt.hash(req.body.password, 10, (err, hash) => {
-          if (err) {
-            return res.status(500).send({
-              msg: err,
-            });
-          } else {
-            
-            db.query(
-              `INSERT INTO users (name, email, password) VALUES ('${
-                req.body.name
-              }', ${db.escape(req.body.email)}, ${db.escape(hash)})`,
-              (err, result) => {
-                if (err) {
-                  throw err;
-                }
-                return res.status(201).send({
-                  msg: "The user has been registerd with us!",
-                });
-              }
-            );
-          }
-        });
-      }
-    }
-  );
-});
-
-router.post("/login", loginValidation, (req, res, next) => {
-  db.query(
-    `SELECT * FROM users WHERE email = ${db.escape(req.body.email)};`,
-    (err, result) => {
-      
-      if (err) {
-        throw err;
-      }
-      if (!result.length) {
-        return res.status(401).send({
-          message: "Email or password is incorrect!",
-        });
-      }
-      
-      bcrypt.compare(
-        req.body.password,
-        result[0]["password"],
-        (bErr, bResult) => {
-          
-          if (bErr) {
-            throw bErr;
-          }
-          if (bResult) {
-            const token = jwt.sign(
-              { id: result[0].id },
-              "the-super-strong-secrect",
-              { expiresIn: "1h" },
-              { algorithm: 'HS256' }
-            );
-            res.co
-            db.query(
-              `UPDATE users SET last_login = now() WHERE id = '${result[0].id}'`
-            );
-            return res.status(200).send({
-              msg: "Logged in!",
-              token,
-              user: result[0],
-            });
-          }
-          return res.status(401).send({
-            message: "Username or password is incorrect!",
-          });
-        }
-      );
-    }
-  );
-});
-
-router.get("/get-user", signupValidation, (req, res, next) => {
-  if (
-    !req.headers.authorization ||
-    !req.headers.authorization.startsWith("Bearer") ||
-    !req.headers.authorization.split(" ")[1]
-  ) {
-    return res.status(422).json({
-      message: "Please provide the token",
-    });
-  }
-
-  const theToken = req.headers.authorization.split(" ")[1];
-  const decoded = jwt.verify(theToken, "the-super-strong-secrect");
-
-  db.query(
-    "SELECT * FROM users where id=?",
-    decoded.id,
-    function (error, results, fields) {
-      if (error) throw error;
-      return res.send({
-        error: false,
-        data: results[0],
-        message: "Fetch Successfully.",
-      });
-    }
-  );
-});
+router.post("/registerUser", registerUser);
+router.post("/loginUser", loginUser);
+router.get("/getUser",  getUser);
 
 router.get("/tasks", getTasks);
 router.get("/tasks/:id", getTaskById);
